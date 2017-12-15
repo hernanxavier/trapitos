0c243beaba77c13b8e29c0f88f48897297274cb1
Xavier <hernanxavierp@gmail.com>
2017-12-15 16:02:23 -0500

    CRUD Modelos ColorTela y Tela

--- tra_base/__manifest__.py
+++ tra_base/__manifest__.py
@@ -3,20 +3,22 @@
     'name': "tra_base",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo en donde se encuentran los CRUDS de tablas básicas para
+        el calculo de costos de los productos de la empresa textil Trapitos""",
 
     'description': """
-        Long description of module's purpose
+        En este modulo se encuentran desarrollados los mantenimientos de tablas que contienen
+        datos para el calculo de costos de la confección de prendas textiles, y que al final permitirán
+        emitir un presupuesto de dicha confección a los clientes de la empresa Trapitos.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xavier Pacheco",
+    'website': "http://www.desarrollo593.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Básica',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,8 +27,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/tra_base_views.xml',
+        'views/tra_base_menu.xml',
+        'views/tra_base_actions.xml',
+        'views/tra_base_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- tra_base/models/models.py
+++ tra_base/models/models.py
@@ -13,3 +13,32 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class tra_ColorTela (models.Model):
+    _name = 'tra.color.tela'
+    _description = 'Registra los colores posibles para las telas'
+    _order = 'name'
+
+    _sql_constraints = [
+        ('unique_codigo','unique(codigo)', u'Otro color ya tiene este código'),
+    ]
+    name = fields.Char(u'Nombre', size = 100, required = True)
+    codigo = fields.Char(u'Código', size = 6, required = True)
+    observacion = fields.Text (u'Observación', size = 200, required = False)
+
+
+class tra_Tela(models.Model):
+    _name = 'tra.tela'
+    _description = u'Registra los valores básicos de la materia prima telas'
+    _order = 'name'
+
+    _sql_constraints = [
+        ('unique_codigo', 'unique(codigo)', u'Otro tipo de tela ya tiene este código'),
+    ]
+    name = fields.Char('Nombre', size = 100, required = True)
+    codigo = fields.Char(u'Código', size = 100, required = True)
+    ancho  = fields.Float('Ancho', size = 6, required = True)
+    color_id = fields.Many2one('tra.color.tela', 'Color', required = True)
+    costo = fields.Float('Costo', size = 6 , required = True)
+    iva = fields.Float('IVA (%)', size =3 , required = True)
+    observacion = fields.Char (u'Observación', size = 200, required = False)
